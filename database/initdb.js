f2c1a09cdcf43d53a557c40faece89bfc12fef2c
yannick DOLLOU <yannick.dollou@axa.fr>
2023-06-25 21:20:14 +0200

    maj docker compose,dockerfile, vérif recuperation du initdb.js

---
+++ database/initdb.js
@@ -0,0 +1,39 @@
+db.notes.deleteMany({})
+db.notes.insertMany([
+    {
+        patientId: 1,
+        observation: "Observation 1",
+        date: new Date()
+    },
+    {
+        patientId: 1,
+        patient: null,
+        observation: "fumeur,taille,cholestérol,vertige",
+        date: new Date()
+    },
+    {
+        patientId: 2,
+        observation: "vertige",
+        date: new Date()
+    },
+    {
+        patientId: 3,
+        observation: "réaction",
+        date: new Date()
+    },
+    {
+        patientId: 4,
+        observation: "microalbumine,anticorps",
+        date: new Date()
+    },
+    {
+        patientId: 5,
+        observation: "fumeur,poids",
+        date: new Date()
+    },
+    {
+        patientId: 6,
+        observation: "fumeur",
+        date: new Date()
+    }
+]);
